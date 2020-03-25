e2d4db29c798ac1192c3d795da804f0a4fc55417
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-03-25 08:32:50 -0700

    Fix JSON output to include all fields

--- outputstructure.go
+++ outputstructure.go
@@ -6,29 +6,44 @@ import (
 	"github.com/ayyaruq/zanarkand"
 )
 
-// IpcStructure - Struct of the fields that IPC packets can have
+// IpcStructure - Struct of fields IPC packets can have
 type IpcStructure struct {
 	zanarkand.GameEventMessage
-	ParsedIpcHeader
-	IpcParameters
-}
-
-// ParsedIpcHeader holds parsed header metadata and environment metadata that makes future parsing easier
-type ParsedIpcHeader struct {
 	Direction string `json:"direction"`
 	Region    string `json:"region"`
 	SubType   string `json:"subType"`
 	SuperType string `json:"superType"`
 	Type      string `json:"type"`
+	IpcParameters
 }
 
-func (ipc *ParsedIpcHeader) MarshalJSON() ([]byte, error) {
-	return json.Marshal(&ParsedIpcHeader{
-		Direction: ipc.Direction,
-		Region:    ipc.Region,
+// MarshalJSON overrides all child JSON serialization methods. Apparently there's a less redundant way to do this, but at the moment I don't care because it works.
+// It also lets me explciitly define field order.
+func (ipc *IpcStructure) MarshalJSON() ([]byte, error) {
+	data := make([]int, len(ipc.Body))
+	for i, b := range ipc.Body {
+		data[i] = int(b)
+	}
+	return json.Marshal(&struct {
+		Opcode    uint16 `json:"opcode"`
+		Type      string `json:"type"`
+		SubType   string `json:"subType"`
+		SuperType string `json:"superType"`
+		Direction string `json:"direction"`
+		ServerID  uint16 `json:"serverID"`
+		Region    string `json:"region"`
+		Timestamp int32  `json:"timestamp"`
+		Data      []int  `json:"data"`
+	}{
+		Opcode:    ipc.Opcode,
+		Type:      ipc.Type,
 		SubType:   ipc.SubType,
 		SuperType: ipc.SuperType,
-		Type:      ipc.Type,
+		Direction: ipc.Direction,
+		ServerID:  ipc.ServerID,
+		Region:    ipc.Region,
+		Timestamp: int32(ipc.Timestamp.Unix()),
+		Data:      data,
 	})
 }
 
