25c4334b5da60374f3186f6cac8b9341bb2f0574
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-03-25 00:47:02 -0700

    Half-fix for struct fields being absent

--- outputstructure.go
+++ outputstructure.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/ayyaruq/zanarkand"
 )
 
-// IpcStructure - Struct of fields IPC packets can have
+// IpcStructure - Struct of the fields that IPC packets can have
 type IpcStructure struct {
 	zanarkand.GameEventMessage
+	ParsedIpcHeader
+	IpcParameters
+}
+
+// ParsedIpcHeader holds parsed header metadata and environment metadata that makes future parsing easier
+type ParsedIpcHeader struct {
 	Direction string `json:"direction"`
 	Region    string `json:"region"`
 	SubType   string `json:"subType"`
 	SuperType string `json:"superType"`
 	Type      string `json:"type"`
-	IpcParameters
+}
+
+func (ipc *ParsedIpcHeader) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&ParsedIpcHeader{
+		Direction: ipc.Direction,
+		Region:    ipc.Region,
+		SubType:   ipc.SubType,
+		SuperType: ipc.SuperType,
+		Type:      ipc.Type,
+	})
 }
 
 // IpcParameters - Holds any IPC struct to be serialized later
