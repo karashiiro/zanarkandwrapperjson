d387417d1f0c0dbf93513d950d2384211e4ba9f0
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-03-26 00:58:30 -0700

    C++ deps make me want to `go die`

--- outputstructure.go
+++ outputstructure.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 
 	"github.com/ayyaruq/zanarkand"
 )
@@ -18,13 +19,13 @@ type IpcStructure struct {
 }
 
 // MarshalJSON overrides all child JSON serialization methods. Apparently there's a less redundant way to do this, but at the moment I don't care because it works.
-// It also lets me explciitly define field order.
+// It also lets me explicitly define field order.
 func (ipc *IpcStructure) MarshalJSON() ([]byte, error) {
 	data := make([]int, len(ipc.Body))
 	for i, b := range ipc.Body {
 		data[i] = int(b)
 	}
-	return json.Marshal(&struct {
+	b1, err := json.Marshal(&struct {
 		Opcode    uint16 `json:"opcode"`
 		Type      string `json:"type"`
 		SubType   string `json:"subType"`
@@ -34,19 +35,30 @@ func (ipc *IpcStructure) MarshalJSON() ([]byte, error) {
 		Region    string `json:"region"`
 		Timestamp int32  `json:"timestamp"`
 		Data      []int  `json:"data"`
-		IpcParameters
 	}{
-		Opcode:        ipc.Opcode,
-		Type:          ipc.Type,
-		SubType:       ipc.SubType,
-		SuperType:     ipc.SuperType,
-		Direction:     ipc.Direction,
-		ServerID:      ipc.ServerID,
-		Region:        ipc.Region,
-		Timestamp:     int32(ipc.Timestamp.Unix()),
-		Data:          data,
-		IpcParameters: ipc.IpcParameters,
+		Opcode:    ipc.Opcode,
+		Type:      ipc.Type,
+		SubType:   ipc.SubType,
+		SuperType: ipc.SuperType,
+		Direction: ipc.Direction,
+		ServerID:  ipc.ServerID,
+		Region:    ipc.Region,
+		Timestamp: int32(ipc.Timestamp.Unix()),
+		Data:      data,
 	})
+	if err != nil {
+		log.Println(err)
+	}
+	b2, err := json.Marshal(ipc.IpcParameters)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// I give it a solid 3/10, with a TODO: make this not a string manip hack
+	s1 := string(b1[:len(b1)-1])
+	s2 := string(b2[1:])
+
+	return []byte(s1 + ", " + s2), nil
 }
 
 // IpcParameters - Holds any IPC struct to be serialized later
