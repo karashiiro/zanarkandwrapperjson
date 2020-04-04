b0f056a20870217026d006c2f238a1d4b1d272ae
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-04-04 15:30:33 -0700

    use dynamicstruct instead of string concatenation

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/karashiiro/ZanarkandWrapperJSON
 go 1.13
 
 require (
+	github.com/Ompluscator/dynamic-struct v1.2.0 // indirect
 	github.com/ayyaruq/zanarkand v0.0.0-20200322114618-b0c322c37487
 	github.com/go-delve/delve v1.4.0 // indirect
 )
--- outputstructure.go
+++ outputstructure.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"strings"
 
+	dynamicstruct "github.com/Ompluscator/dynamic-struct"
 	"github.com/ayyaruq/zanarkand"
 )
 
@@ -20,14 +20,14 @@ type IpcStructure struct {
 	IpcMessageFields
 }
 
-// MarshalJSON overrides all child JSON serialization methods. Apparently there's a less redundant way to do this, but at the moment I don't care because it works.
-// It also lets me explicitly define field order.
+// MarshalJSON overrides all child JSON serialization methods.
 func (ipc *IpcStructure) MarshalJSON() ([]byte, error) {
 	data := make([]int, len(ipc.Body))
 	for i, b := range ipc.Body {
 		data[i] = int(b)
 	}
-	b1, err := json.Marshal(&struct {
+
+	mainComponent := &struct {
 		Opcode    uint16 `json:"opcode"`
 		Type      string `json:"type"`
 		SubType   string `json:"subType"`
@@ -47,20 +47,11 @@ func (ipc *IpcStructure) MarshalJSON() ([]byte, error) {
 		Region:    ipc.Region,
 		Timestamp: int32(ipc.Timestamp.Unix()),
 		Data:      data,
-	})
-	if err != nil {
-		log.Println(err)
-	}
-	b2, err := json.Marshal(ipc.IpcMessageFields)
-	if err != nil {
-		log.Println(err)
 	}
 
-	// I give it a solid 3/10, with a TODO: make this not a string manip hack
-	s1 := string(b1[:len(b1)-1])
-	s2 := string(b2[1:])
+	mergedStruct := dynamicstruct.MergeStructs(mainComponent, ipc.IpcMessageFields).Build()
 
-	return []byte(s1 + ", " + s2), nil
+	return json.Marshal(mergedStruct)
 }
 
 func jsifyString(str string) string {
